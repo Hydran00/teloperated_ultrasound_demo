1bcd2deff2e89e1ce6601d45e46eb1dcac3508d7
hydran00 <davide.nardi-1@unitn.it>
2026-02-03 18:20:33 +0100

    added clarius

--- leader/entrypoint.sh
+++ leader/entrypoint.sh
@@ -3,3 +3,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/haptic_interface_ROS2/src/haption
 source haptic_interface_ROS2/install/setup.bash
 source kuka_lbr_control/install/setup.bash
 source zed_ws/install/setup.bash
+source clarius_ws/install/setup.bash
